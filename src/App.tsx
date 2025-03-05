80d773848e7d09fa13f5d9de0ccacb4f2f3bf786
tndls0720 <tndls154154@gmail.com>
2025-03-05 11:49:36 +0900

    ✨ UserSignup 라우터 설정
    회원가입 페이지 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserSignupForm from "./pages/UserSignup";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main>
-        <h1>메인컨텐츠영역</h1>
-      </main>
+      <Routes>
+        <Route path="/UserSignup" element={<UserSignupForm />} />
+      </Routes>
       <Footer />
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/UserSignup.tsx
@@ -0,0 +1,8 @@
+
+export const UserSignupForm = () => {
+  return (
+    <div>UserSignup</div>
+  )
+}
+
+export default UserSignupForm;
