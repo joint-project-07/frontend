1a2ad4cedf221023c52bba46995ee5455f1720da
tndls0720 <tndls154154@gmail.com>
2025-03-05 13:35:09 +0900

    📝 필드명, 파일명 수정
    필드명, 파일명 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserSignupForm from "./pages/UserSignup";
+import UsersSignupForm from "./pages/UsersSignup";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/UserSignup" element={<UserSignupForm />} />
+        <Route path="/UsersSignup" element={<UsersSignupForm />} />
       </Routes>
       <Footer />
     </BrowserRouter>
